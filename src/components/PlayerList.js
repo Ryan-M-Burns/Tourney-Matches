26d293d5ec88b06c14f6a63a07d76bc786d4a84b
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-02 22:52:50 -0700

    get all players'

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -3,14 +3,23 @@ import Player from './Player';
 
 function PlayerList(props) {
 
-  const onePlayer = props.playerData[0];
+  const players = props.playerData.map(player => {
+    return (
+      <Player
+        firstName={player.firstName}
+        lastName={player.lastName}
+        gamerTag={player.gamerTag} />
+    );
+  });
+
+
 
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
-      <Player gamerTag={onePlayer.gamerTag} firstName={onePlayer.firstName} lastName={onePlayer.lastName} wins={onePlayer.wins} />
+      {players}
     </section>
   );
-}
+};
 
 export default PlayerList;
