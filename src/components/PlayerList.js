59f1b12b66558bdbf5e7b698d14c781c14ab2ee7
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-02 22:40:29 -0700

    fix faulty code

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,15 +1,16 @@
+import React from 'react';
 import Player from './Player';
 
-const PlayerList = props => {
+function PlayerList(props) {
 
-  const onePlayer = props.PlayerData[0];
+  const onePlayer = props.playerData[0];
 
   return (
     <section className="PlayerList">
-      <h1>Current Participating Players</h1>
+      <h1>Current participating players</h1>
       <Player gamerTag={onePlayer.gamerTag} firstName={onePlayer.firstName} lastName={onePlayer.lastName} wins={onePlayer.wins} />
     </section>
   );
-};
+}
 
 export default PlayerList;
