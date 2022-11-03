f60165e34dfe3fb88128ee85b0eb869a7f5eae5e
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-02 21:42:45 -0700

    implement props

--- src/component/PlayerList.js
+++ src/component/PlayerList.js
@@ -6,11 +6,13 @@ import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers';
 
 const PlayerList = props => {
   const playerDataArray = preparePlayerData(playerData);
-  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+  const parsePlayerData = addWinsToPlayers(playerDataArray, matchData);
+  const onePlayer = parsePlayerData[0];
+
   return (
     <section className="PlayerList">
       <h1>Current Participating Players</h1>
-      <Player />
+      <Player gamerTag={onePlayer.gamerTag} firstName={onePlayer.firstName} lastName={onePlayer.lastName} wins={onePlayer.wins} />
     </section>
   );
 };
