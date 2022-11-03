923a8d9f03bac6d29a0c82e24a557d893630817f
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-11-02 18:01:44 -0700

    clear App.js and create skeleton for Player.js

--- src/component/Player.js
+++ src/component/Player.js
@@ -1,6 +1,17 @@
-<article className="Player">
-  <h1>John <span>Secret</span> Doe</h1>
-  {/* To be shown when there are no wins */}<h2 className="zero">Currently with no wins</h2>
-  {/* To be shown when there is 1 win */}<h2 className="zero">Currently at 1 win</h2>
-  {/* To be shown when there are more than 1 win */}<h2 className="zero">Currently at 1+ wins</h2>
-</article>;
+import React from "react";
+
+const Player = () => {
+  return (
+    <article className="Player">
+      <h1>
+        John <span>Secret</span> Doe
+      </h1>
+      {/* To be shown when there are no wins */}
+      <h2 className="zero">Currently with no wins</h2>
+      {/* To be shown when there is 1 win */}
+      <h2 className="zero">Currently at 1 win</h2>
+      {/* To be shown when there are more than 1 win */}
+      <h2 className="zero">Currently at 1+ wins</h2>
+    </article>
+  );
+};
